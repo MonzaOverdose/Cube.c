0e2ce74e4254ce3db5c93b4c34beda1c094527c3
Bruno T <107083804+MonzaOverdose@users.noreply.github.com>
2023-05-02 22:23:58 -0300

    Add files via upload

--- cube.c
+++ cube.c
@@ -3,66 +3,101 @@
 #include <string.h>
 #include <unistd.h>
 
-float A, B, C,;
+flaot A,B,C;
 
 float cubeWidth = 20;
-int width = 160, height = 44;
+int wifth =  160, height = 44;
 float zBuffer[160 * 44];
 char buffer[160 * 44];
-int backgroundASCCI = '.';
+int backgroundASCCICode = '.';
 int distanceFromCam = 100;
 float horizontalOffset;
-flaot K1 = 40;
+float K1 = 40;
 
-float incrementSpeed =  0.6;
+flaot incremenSpeed = 0.6;
 
 float x, y, z;
 float ozz;
-int xp; yp;
+int xp, yp;
 int idx;
 
-float calculateX(int i, int,j intk) {
-     return j * cos(A) * cos(C) + k sin(A) * cos(C) -
-            j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(B);
-            i * cos(B) * sin(C);
-
-
-
-
-
+float calculateX(int i, int ,j intk) {
+    return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
+           j * cos(A) * sin(C) * subn(C) + k * sin(A) * sin(C) + i * cos(B) * co(C);          
 }
 
-float calculateZ(int i, int,j int k) {
-    return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);    
+float calculateY(int i, int j, int k) {
+    return j * cos(A) * cos(C) + k * sin(A) * cos(C) -
+           j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) -
+           i * cos(B) * sin(C);
 }
 
-void calculateForSurface(float cubeX, cubeY, cubeZ);
-x = calculateX(cubeX, cubeY, cubeZ);
-y = calculateY(cubeX, cubeY, cubeZ);
-z - calculateZ(cubeX, cubeY, cubeZ)
+float calculateZ(int i, int j, int k) {
+    return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
+}
 
-ooz = 1 / z;
+void calculateForSurface(float cubeX, float cubeY, flaot cubeZ,int ch) {
+    x = calculateX(cubeX, cubeY, cubeZ);
+    y = calculateY(cubeX, cubeY, cubeZ);
+    z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
 
-xp = (int)(width / 2 + horizontalOffset + K1 * ozz * x * 2);
-yp = (int)(height / 2 + K1 * ooz * y);
+    ozz = 1 / z;
 
-idx = xp + yp * width;
-if (idx >= 0 && idx < width * height) {
-    if (ooz > zBuffer[idx]) {
-        zBuffer[idx]{
-            buffer[idx] = ch;
+    xp = (int)(width / 2 + horizontalOffset + K1 * ozz * x * 2);
+    yp = (int)(height / 2 + K1 * ozz * y);
+
+    id = xp + yp * width;
+    if (idx >= 0 && idx < width * height) {
+        if (ozz > zBuffer[idx]) {
+
         }
     }
+
 }
 
 int main() {
-    printf("\x1b[2J");
+    printf("\x1b[2J")
     while (1) {
         memset(buffer, backgroundASCCICode, width * height);
-        memset(zBuffer, 0, width * height);
+        memset(zBuffer, 0 , width * height * 4);
         cubeWidth = 20;
         horizontalOffset = -2 * cubeWidth;
-        // firs cube
-        for (float cubeX = )
+        // primeiro cubo 
+        for float (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+         for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+         cubeY = += incrementSpeed) {
+            calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+            calculateForSurface(cubeWidth,cubeY, cubeX, '$');
+            calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+            calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+            calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+           calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+          }
+      }
+cubeWidth = 10;
+horizontalOffset = 1 * cubeWidth
+// segundo cubo 
+for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incremenSpeed) {
+    for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+    cubeY += incrementSpeed) {
+calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+    }     
+}
+printf("\x1b[H");
+for (int k = o; k < width * height; k++) {
+    putchar(k % width ? buffer[k] : 10);
+
+}
+
+A += 0.05;
+B += 0.05;
+C += 0.05;
+usleep(8000 * 2);
     }
+    return 0;
 }
